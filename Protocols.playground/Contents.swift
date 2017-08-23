9288a5f93c29140c47bda27cc560046a083f734d
Tomek Buslowski <tbuslowski@gmail.com>
2017-08-23 14:17:00 +0200

    protocol examples

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -2,16 +2,31 @@
 
 import UIKit
 
+protocol TabularDataStructure {
+    var numberOfRows: Int { get }
+    var numberOfColumns: Int { get }
+    
+    func labelForRow(_ row: Int) -> String
+    func labelForColumn(_ column: Int) -> String
+    
+    func itemForRow(_ row: Int, column: Int) -> Int
+}
+
+
 func padding(amount: Int) -> String {
     var paddingString = ""
-    for _ in 0..<amount {
+    for _ in 0 ..< amount {
         paddingString += " "
     }
     return paddingString
 }
 
 
-func printTable(columnLabels: [String], rowLabels: [String], data: [[Int]] ) {
+func printTable(dataSource: TabularDataStructure & CustomStringConvertible) {
+    
+    let rowLabels = (0..<dataSource.numberOfRows).map{ dataSource.labelForRow( $0 ) }
+    let columnLabels = (0..<dataSource.numberOfColumns).map{ dataSource.labelForColumn( $0 ) }
+    
     // Size of label of each row
     let rowLabelWidths = rowLabels.map { $0.characters.count }
     
@@ -28,13 +43,16 @@ func printTable(columnLabels: [String], rowLabels: [String], data: [[Int]] ) {
         firstRow += columnHeader
         columnWidths.append(columnHeader.characters.count)
     }
+    
+    print("Tabela: \(dataSource.description)")
     print(firstRow)
     
-    for (i, row) in data.enumerated() {
+    for i in 0 ..< dataSource.numberOfRows {
         let paddingAmount = maxRowLabelWidth - rowLabelWidths[i]
         var out = rowLabels[i] + padding(amount: paddingAmount) + " |"
         
-        for (j, item) in row.enumerated() {
+        for j in 0 ..< dataSource.numberOfColumns {
+            let item = dataSource.itemForRow(i, column: j)
             let itemString = " \(item) |"
             let paddingAmount = columnWidths[j] - itemString.characters.count
             out += padding(amount: paddingAmount) + itemString
@@ -43,12 +61,115 @@ func printTable(columnLabels: [String], rowLabels: [String], data: [[Int]] ) {
     }
 }
 
-let columnLabels = ["Wiek", "Lata doświadczenia"]
-let rowLabels = ["Tomek", "Kuba", "Patryk"]
-let data = [
-    [30, 6],
-    [40, 18],
-    [50, 20]
-]
+struct Person {
+    let name: String
+    let age: Int
+    let yearsOfExperience: Int
+}
 
-printTable(columnLabels: columnLabels, rowLabels: rowLabels, data: data)
+struct Department: TabularDataStructure, CustomStringConvertible {
+    let name: String
+    var people = [Person]()
+    
+    var description: String {
+        get {
+            return "Departament (\(name))"
+        }
+    }
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    mutating func addPerson(person: Person) {
+        people.append(person)
+    }
+    
+    var numberOfRows: Int {
+        get {
+            return people.count
+        }
+    }
+    
+    var numberOfColumns: Int {
+        get {
+            return 2
+        }
+    }
+    
+    func labelForRow(_ row: Int) -> String {
+        return people[row].name
+    }
+    
+    func labelForColumn(_ column: Int) -> String {
+        switch column {
+        case 0:
+            return "Wiek"
+        case 1:
+            return "Lata doświadczenia"
+        default:
+            fatalError("Nieprawidłow kolumna!")
+        }
+    }
+    
+    func itemForRow(_ row: Int, column: Int) -> Int {
+        let person = people[row]
+        switch column {
+        case 0:
+            return person.age
+        case 1:
+            return person.yearsOfExperience
+        default:
+            fatalError("Nieprawidłowa kolumna!")
+        }
+    }
+}
+
+var department = Department(name: "Inżynieria")
+department.addPerson(person: Person(name: "Tomek", age: 23, yearsOfExperience: 2))
+department.addPerson(person: Person(name: "Patryk", age: 30, yearsOfExperience: 7))
+department.addPerson(person: Person(name: "Kuba", age: 17, yearsOfExperience: 3))
+
+printTable(dataSource: department)
+
+
+
+
+
+protocol Toggleable {
+    mutating func toggle()
+}
+
+enum Lightbulb: Toggleable {
+    case On
+    case Off
+    
+    mutating func toggle() {
+        switch self {
+        case .On:
+            self = .Off
+        case .Off:
+            self = .On
+        }
+    }
+}
+
+class Candle: Toggleable {
+    var isLighting = false
+    
+    // Class don't need mutating keyword because in Class we pass Reference.
+    func toggle() {
+        switch isLighting {
+        case true:
+            isLighting = false
+        case false:
+            isLighting = true
+        }
+    }
+}
+
+let candle = Candle()
+candle.toggle()
+candle.isLighting
+candle.toggle()
+candle.isLighting
